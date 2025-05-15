2cc9f16359280b491d4c693398582ad584df4ce9
seif <seiftarek090@gmail.com>
2025-05-15 15:00:45 +0300

    Refactor code structure for improved readability and maintainability

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  // Add your component properties here
+  selectedUser = DUMMY_USERS[randomIndex];
+  
+  // Add your component methods here
+
+  // Add your component logic here
+
+
+
+}
