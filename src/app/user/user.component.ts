197af0c55e7d34a564da53ff4448019cf46bbdc9
seif <seiftarek090@gmail.com>
2025-05-15 15:12:06 +0300

    feat: implement user selection functionality and update template

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
+import { CommonModule } from '@angular/common';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
   // Add your component properties here
   selectedUser = DUMMY_USERS[randomIndex];
-  
+
+
   // Add your component methods here
+  get imagePath(){
+    return 'assets/' + this.selectedUser.avatar
+  }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
 
   // Add your component logic here
 
