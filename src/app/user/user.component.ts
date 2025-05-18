73086e3e760f0a76d20ce74e4b1671cf552131ab
seif <seiftarek090@gmail.com>
2025-05-18 12:56:01 +0300

    feat: create Task and User models to define data structures for tasks and users

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,13 @@
 import { Component, EventEmitter, Input, Output} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { User } from './user.model';
 
 // type User = {
 //   id: string;
 //   name: string;
 //   avatar: string;
 // }
-interface User {
-  id: string;
-  name: string;
-  avatar: string;
-}
+
 @Component({
   selector: 'app-user',
   standalone: true,
