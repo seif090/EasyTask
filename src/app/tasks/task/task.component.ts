4c31370b23f20925f317ca0781b91e0cb76dc136
seif <seiftarek090@gmail.com>
2025-05-18 13:11:27 +0300

    feat: implement task completion functionality with event emission and filtering

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from './task.model';
 
 
@@ -11,5 +11,10 @@ import { Task } from './task.model';
 })
 export class TaskComponent {
   @Input({required: true}) task!: Task;
+  @Output() complete = new EventEmitter<string>();
+
+  onCompleteTask() {
+    this.complete.emit(this.task.id);
+  }
 
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -60,4 +60,8 @@ get selectedUserTasks(){
   return this.tasks.filter(task => task.userId === this.userId);
 }
 
+onCompleteTask(id: string) {
+  this.tasks = this.tasks.filter(task => task.id !== id);
+}
+
 }
